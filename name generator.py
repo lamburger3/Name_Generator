48cdcf45ce204f439667a20668f1f7880f3b9f32
lamburger3 <lamburger3@gmail.com>
2017-12-03 21:36:06 -0800

    Thanks to my good friend Matt, you can now generate as many names as you'd like.

--- name generator.py
+++ name generator.py
@@ -5,28 +5,41 @@ first_name_2 = ("ulan", "ulas", "ulus" ,"ulan", "oran", "lero", "udan", "igan", 
 
 last_name_1 = ("Rom", "Kor", "Sep", "Sor", "Ren", "Jarl", "Kren", "Balg", "Boro", "Orog", "Kora", "Imu")
 last_name_2 = ("ulan", "ulas", "ulus" ,"ulan", "oran", "lero", "udan", "igan", "oro", "on", "uru", "korag", "lan")
+'''
+This function takes as a parameter the number of names to generate and generates and prints out that number of names
+'''
+def random_name(name_amount):
+    try:
+        for x in range(0, name_amount):
+            fullfirstname = random.choice(first_name_1)+""+random.choice(first_name_2)
+            fulllastname = random.choice(last_name_1)+""+random.choice(last_name_2)
+            fullname = fullfirstname+ " " +fulllastname
+            print fullname
+    except:
+       pass #Does nothing
 
-group = list()
-
-def random_name():
-    for x in range(0, name_amount):
-        fullfirstname = random.choice(first_name_1)+""+random.choice(first_name_2)
-        fulllastname = random.choice(last_name_1)+""+random.choice(last_name_2)
-        fullname = fullfirstname+ " " +fulllastname
-    print fullname
-    user_input()
-    random_name()
-
-
+'''
+This function asks the user to enter a numerical value and returns that value
+'''
 def user_input():
-    global name_amount
     try:
         name_amount = input("How many names would you like generated? ")
+        return name_amount
     except:
         print("Please enter a number: ")
-        user_input()
-user_input()
-random_name()
+        
+'''
+This function takes the numerical input from user_input and stores it in the "x" variable, then calls random_name with "x" as a parameter
+'''
+def main():
+    while True:
+        x = user_input()
+        random_name(x)
+
+'''
+Calls the main function and starts the program
+'''
+main()
 
 
 
